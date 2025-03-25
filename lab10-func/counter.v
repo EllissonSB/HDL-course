aa3766248618584c408b292e4401ecb88fde9798
EllissonSB <ellisson-silva@hotmail.com>
2025-03-24 21:42:22 -0300

    On branch main

--- lab10-func/counter.v
+++ lab10-func/counter.v
@@ -11,12 +11,37 @@ module counter
   output reg  [WIDTH-1:0] cnt_out 
  );
 
-  
 //////////////////////////////////////////////////////////////////////////////
 //TO DO: DEFINE THE COUNTER COMBINATIONAL LOGIC using FUNCTION AS INSTRUCTED//
 //////////////////////////////////////////////////////////////////////////////
+function [WIDTH-1:0] cnt_func;
+  input rst;
+  input load;
+  input enab;
+  input [WIDTH-1:0] cnt_in,cnt_out;
+  integer cont;
+  begin
+    if(rst == 1) begin
+      cont = 0;
+      cnt_func = 0;
+    end else begin
+      if(enab == 1) begin
+        if(load) begin
+          cont = cnt_in;
+          cnt_func = cont;
+        end else begin
+          cont = cont + 1;
+          cnt_func = cont;
+        end
+      end else begin
+        cnt_func = 0;
+      end
+    end
+  end
+endfunction
 
-  always @(posedge clk)
-     cnt_out <= cnt_func (rst, load, enab ,cnt_in, cnt_out); //function call
+always @(posedge clk) begin
+  cnt_out <= cnt_func(rst, load, enab, cnt_in,cnt_out); // function call
+end
 
 endmodule
