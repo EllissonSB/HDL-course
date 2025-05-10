b07b0eb992ceaf3fb74dbd87e47618b84a619fa6
Ellisson da Silva Barbosa <57837838+EllissonSB@users.noreply.github.com>
2025-05-10 10:03:18 -0300

    Implement combinational logic for priority encoder, latch, D flip-flop, and bus driver

--- lab13-lib/lib.v
+++ lab13-lib/lib.v
@@ -25,6 +25,18 @@ module priority7 ( y, a ) ;
   output reg  [2:0] y ;
   input  wire [7:1] a ;
 
+  always @(*) begin
+    case (a)
+      7'bxxxxxx1 : y = 3'b001 ;
+      7'bxxxxx10 : y = 3'b010 ;
+      7'bxxxx100 : y = 3'b011 ;
+      7'bxxx1000 : y = 3'b100 ;
+      7'bxx10000 : y = 3'b101 ;
+      7'bx100000 : y = 3'b110 ;
+      7'b1000000 : y = 3'b111 ;
+      default     : y = 3'b000 ;
+    endcase
+  end
 
 endmodule
 `endif
@@ -42,10 +54,26 @@ module latchrs ( q, e, d, r, s ) ;
   output reg  q ;
   input  wire e, d, r, s ;
 
+  always @(*) begin: latch 
+    if (r == 1'b1 && s == 1'b0) begin
+       q<= 1'b1;
+    end
+    else if(s == 1'b0 && r == 1'b0) begin
+      q <= 1'b1;
+    end
+     else if (s == 1'b1 && r == 1'b1 && d == 1'b1) begin
+      q<= 1'b1;
+    end
+    else if (s == 1'b1 && (r == 1'b0 || r == 1'b1) ) begin
+      q<= 1'b0;
+    end
+    else begin
+      q<= 1'b0;
+    end
+  end
+
 // do not remove comment below
 // cadence async_set_reset "s, r"
-
-
 
 endmodule
 `endif
@@ -61,6 +89,24 @@ module dffrs ( q, c, d, e, r, s ) ;
 
   output reg  q ;
   input  wire c, d, e, r, s ;
+
+  always @(*) begin
+    if (r == 1'b1 && s == 1'b0) begin
+       q<= 1'b1;
+    end
+    else if(s == 1'b0 && r == 1'b0) begin
+      q <= 1'b1;
+    end
+     else if (s == 1'b1 && r == 1'b1 && d == 1'b1) begin
+      q<= 1'b1;
+    end
+    else if (s == 1'b1 && (r == 1'b0 || r == 1'b1) ) begin
+      q<= 1'b0;
+    end
+    else begin
+      q<= 1'b0;
+    end
+  end
 
 
 endmodule
@@ -78,7 +124,14 @@ module drive8 ( y, a, e ) ;
   output reg  [7:0] y ;
   input  wire [7:0] a ;
   input  wire       e ;
-
+  always @(*) begin
+    if (e) begin
+      y <= a;
+    end
+    else begin
+      y <= 8'bz;
+    end
+  end
 
 endmodule
 `endif
